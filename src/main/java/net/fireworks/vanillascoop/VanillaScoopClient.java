cf5d454de55b3b210903d27d0488b075b47440a7
Michael Long <longmichael97@gmail.com>
2023-02-24 18:59:18 -0800

    Updating Start to 1.19.3

---
+++ src/main/java/net/fireworks/vanillascoop/VanillaScoopClient.java
@@ -0,0 +1,9 @@
+package net.fireworks.vanillascoop;
+
+import net.fabricmc.api.ClientModInitializer;
+public class VanillaScoopClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/fireworks/vanillascoop/VanillaScoopDataGenerator.java
@@ -0,0 +1,11 @@
+package net.fireworks.vanillascoop;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VanillaScoopDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
